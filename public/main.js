85072a49cddacd99999e3f0fd7e8e073a30dc399
Vincent Alfarieco <vincent111232@gmail.com>
2024-01-05 18:05:47 +0700

    fix update method with params

--- public/main.js
+++ public/main.js
@@ -13,10 +13,32 @@ const addData = () => {
   backButton.style.display = 'block';
 };
 
-const updateData = () => {``
+const updateData = () => {
   editData.style.display = 'block';
   button.style.display = 'none';
   backButton.style.display = 'block';
-}
+};
 
+document.addEventListener('DOMContentLoaded', function () {
+  const editButtons = document.querySelectorAll('.edit-btn');
 
+  editButtons.forEach((button) => {
+    button.addEventListener('click', function (event) {
+      event.preventDefault();
+      const id = this.dataset.id;
+      history.pushState(null, null, `/edit/${id}`);
+      console.log(`URL updated to /edit/${id}`);
+
+      let pathName = window.location.pathname;
+      console.log(pathName);
+
+      const extractIdFromUrl = (path) => {
+        const splitedUrl = path.split('/');
+        return splitedUrl[2];
+      };
+
+      const extractedUrl = extractIdFromUrl(pathName);
+      editData.action = `/edit/${extractedUrl}`;
+    });
+  });
+});
